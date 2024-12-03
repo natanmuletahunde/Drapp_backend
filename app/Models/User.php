eddbc599daa7b6ccba1309cfeae0a3899a1af367
pivnati <natanmuleta911@gmail.com>
2024-12-02 20:24:07 -0700

    make that the doctor and user_details have one relationship with the user

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,12 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function doctor(){
+         return $this->hasOne(Doctor::class, 'doc_id');
+    }
+    public function user_details(){
+        return $this->hasOne(UserDetails::class, 'user_id');
+   }
 
     /**
      * Get the attributes that should be cast.
