7706ebe62ef61a4a979a821a9bab3f0474c30288
pivnati <natanmuleta911@gmail.com>
2024-12-04 19:31:37 -0700

    create the contorlleruser and make connection in the api route in the routes foler

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Livewire\Attributes\Validate;
 
 class UserController extends Controller
 {
@@ -15,11 +19,21 @@ class UserController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * login.
      */
-    public function create()
+    public function login(Request $request )
     {
-        //
+          $request->validate([
+         'email'=>'required|email',
+         'password'=>'required',
+          ]);
+          $user = User::where('email' , $request->email)->first();
+          if(!$user|| !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+            return $user->createToken($request->email)->plainTextToken;
+          }
     }
 
     /**
