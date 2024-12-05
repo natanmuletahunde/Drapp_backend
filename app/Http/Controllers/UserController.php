b96787906668f1499833a85189cf87a94c0a7eb9
pivnati <natanmuleta911@gmail.com>
2024-12-04 20:37:06 -0700

    make the usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,20 +21,27 @@ class UserController extends Controller
     /**
      * login.
      */
-    public function login(Request $request )
-    {
-          $request->validate([
-         'email'=>'required|email',
-         'password'=>'required',
-          ]);
-          $user = User::where('email' , $request->email)->first();
-          if(!$user|| !Hash::check($request->password, $user->password)){
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
-            return $user->createToken($request->email)->plainTextToken;
-          }
+    public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
     }
+
+    return response()->json([
+        'token' => $user->createToken($request->email)->plainTextToken,
+    ]);
+}
+
+    
 
     /**
      * Store a newly created resource in storage.
