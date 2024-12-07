359d084e4f5d957a2b08600bda544c5d397854e2
pivnati <natanmuleta911@gmail.com>
2024-12-06 21:15:33 -0700

    modify the api.php and create the usercontroller in the controller folder

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointments;
+use App\Models\User;
+use App\Models\Doctor;
+use App\Models\UserDetails;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return all data
+    }
+
+    /**
+     * loign.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $reqeust)
+    {
+        //validate incoming inputs
+        $reqeust->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        //check matching user
+        $user = User::where('email', $reqeust->email)->first();
+
+        //check password
+        if(!$user || ! Hash::check($reqeust->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email'=>['The provided credentials are incorrect'],
+            ]);
+        }
+
+        //then return generated token
+        return $user->createToken($reqeust->email)->plainTextToken;
+    }
+
+    /**
+     * register.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming inputs
+        $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'type'=>'user',
+            'password'=>Hash::make($request->password),
+        ]);
+
+        $userInfo = UserDetails::create([
+            'user_id'=>$user->id,
+            'status'=>'active',
+        ]);
+
+        return $user;
+
+
+    }
+
+    /**
+     * update favorite doctor list
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeFavDoc(Request $request)
+    {
+
+        $saveFav = UserDetails::where('user_id',Auth::user()->id)->first();
+
+        $docList = json_encode($request->get('favList'));
+
+        //update fav list into database
+        $saveFav->fav = $docList;  //and remember update this as well
+        $saveFav->save();
+
+        return response()->json([
+            'success'=>'The Favorite List is updated',
+        ], 200);
+    }
+
+    /**
+     * logout.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(){
+       
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\UserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::post('/login', [UserController::class, 'login']);  // Removed space between 'login' and 'login'
+
+
+Route::post('/login', [App\Http\Controllers\UsersController::class, 'login']);
+Route::post('/register', [App\Http\Controllers\UsersController::class, 'register']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
